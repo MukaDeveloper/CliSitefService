42c66b1a21d8b0d8eac712a38afb6856c3b1a3f0
MukaDeveloper <muka.bertoni@gmail.com>
2024-06-12 15:00:17 -0300

    interface startTransaction

--- src/services/startTransaction/startTransaction.ts
+++ src/services/startTransaction/startTransaction.ts
@@ -1,3 +1,5 @@
-export declare class StartTransaction {
+import { IStartTransaction } from "../../interfaces";
 
+export function start(data: IStartTransaction) {
+    return 'START TRANSACTION';
 }
---
+++ src/shared/utils.ts
@@ -0,0 +1,7 @@
+export class GlobalConfig {
+    public agente_uri: string;
+
+    constructor() {
+        this.agente_uri = 'https://127.0.0.1/agente/clisitef';
+    }
+}
